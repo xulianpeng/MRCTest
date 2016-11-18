4cd4f86a5b3ad3e00a1c19bdf84b86d8acf98db0
xulianpeng <xulianpeng@haodf.com>
2016-11-18 12:37:03 +0800

    Initial Commit

---
+++ MRCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRCTest
+//
+//  Created by xulianpeng on 2016/11/18.
+//  Copyright © 2016年 xulianpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MRCTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRCTest
+//
+//  Created by xulianpeng on 2016/11/18.
+//  Copyright © 2016年 xulianpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
